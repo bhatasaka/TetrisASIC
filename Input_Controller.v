0c9e584061377f3da4e5589e97d92c9ccd9f05f4
jamisonb <jamisonb@lab1-18.eng.utah.edu>
2019-11-05 22:04:18 -0700

    Just need to finish up forming output to Game_Controller from button input.

--- Input_Controller.v
+++ Input_Controller.v
@@ -2,10 +2,11 @@ module Input_Controller (
 
     input clk, // 40Mhz clock
     //input reset,
-    //input button_data,
+    input button_data_in,
     output latch_tb,
     output slow_clk_tb,
-    output reg pulse
+    output pulse_tb,
+    output reg [3:0] button_data_out
 
     //output reg slow_clk
 
@@ -13,59 +14,123 @@ module Input_Controller (
     // 40Mhz clock to 60 hertz clock
     reg [18:0] slow_clk_counter = 19'd0;
     reg [18:0] latch_clk_counter = 19'd0;
-    //reg [18:0] pulse_clk_counter = 19'd0;
+    reg [18:0] pulse_clk_counter = 19'd0;
     reg slow_clk = 1'b0;
     //reg pulse_active = 1'b0;
     reg latch = 1'b0;
-    //pulse = 1'b0;
+    reg pulse = 1'b0;
     assign slow_clk_tb = slow_clk;
     assign latch_tb = latch;
+    assign pulse_tb = pulse;
     
     always@(posedge clk) // posedge every 25 ns
     begin
         slow_clk_counter <= slow_clk_counter + 19'd1;
-        //latch_clk_counter <= latch_clk_counter + 1'b1;
-        //pulse_clk_counter <= pulse_clk_counter + 1'b1;
-        if(slow_clk_counter == 19'd333333) //25ns *666666 = 16.7ms then divide by 2 for when to flip from low to high, high to low. 
-        begin
-            if (slow_clk == 1'b0) begin
-                latch <= 1'b1;
+        case (slow_clk_counter)
+            19'd480: begin
+                latch <= 1'b0;
             end
-            //latch_clk_counter <= 19'd0;
-            slow_clk <= ~slow_clk;
-            slow_clk_counter <= 19'd0;
-        end
-        if (slow_clk_counter == 19'd480) 
-        begin
-            latch <= 1'b0;    
-        end
+            19'd720: 
+            begin
+                if (button_data_in == 1'b0) // A button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd1200:
+            begin
+                if (button_data_in == 1'b0) // B button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd1680:
+            begin
+                if (button_data_in == 1'b0) // Select button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd2160:
+            begin
+                if (button_data_in == 1'b0) // Start button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd2640:
+            begin
+                if (button_data_in == 1'b0) // Up button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd3120:
+            begin
+                if (button_data_in == 1'b0) // Down button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd3600:
+            begin
+                if (button_data_in == 1'b0) // Left button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd4080:
+            begin
+                if (button_data_in == 1'b0) // Right button pressed
+                begin
+                    
+                end
+                if (slow_clk == 1'b1) 
+                begin
+                    pulse <= 1'b1;
+                end
+            end
+            19'd960, 19'd1440, 19'd1920, 19'd2400, 19'd2880, 19'd3360, 19'd3840, 19'd4320: 
+            begin
+                pulse <= 1'b0;
+            end
+            19'd333333: // 60 Hz clock
+            begin
+                if (slow_clk == 1'b0) 
+                begin
+                    latch <= 1'b1;
+                end
+                slow_clk <= ~slow_clk;
+                slow_clk_counter <= 19'd0;
+            end
+        endcase
     end
-
-
-
-        // MAKE A 6us BASED CLOCK FOR PULSE and LATCH?
-
-
-    // always@(negedge slow_clk) // posedge every 16.7ms      *do if(slow_clk == 1) in first always block if this creates an error*
-    // begin
-          
-    //     latch <= 1'b0;                  //posedge of latch pulse
-                                      //put this at end of always block? 
-        // if(latch_clk_counter == 19'd480) //25ns * 480 = 12us 
-        // begin
-        //     latch <= 1'b0;              //negedge of latch pulse after 12us
-        // end
-        // if (latch_clk_counter == 19'd720) // send the pulse after 18us
-        // begin
-        //     pulse_clk_counter <= 19'd0 
-        //     if (pulse_active == 1) 
-        //     begin
-        //         pulse <= ~pulse;     // HERE MAKE PULSE SIGNAL
-        //     end
-        //     else
-        //     begin
-        //         pulse <= 1'b0;
-        //     end
-        // end
-    //end
 endmodule
