09c86e682a1b0bd47d7b82bf81863a5b597db983
jamisonb <jamisonb@lab1-18.eng.utah.edu>
2019-11-13 23:19:14 -0700

    Modifications to input controller

--- Input_Controller.v
+++ Input_Controller.v
@@ -1,129 +1,141 @@
 module Input_Controller (
 
-    input clk, // 40Mhz clock
+    input clk, // 50Mhz clock
     //input reset,
     input button_data_in,
     output latch_tb,
     output slow_clk_tb,
     output pulse_tb,
-    output reg [3:0] button_data_out
+    output [3:0] button_data_out_tb
 
     //output reg slow_clk
 
 );
-    // 40Mhz clock to 60 hertz clock
+    // 50Mhz clock to 60 hertz clock
     reg [18:0] slow_clk_counter = 19'd0;
     reg [18:0] latch_clk_counter = 19'd0;
     reg [18:0] pulse_clk_counter = 19'd0;
+    reg button_lock = 1'b1;
+
     reg slow_clk = 1'b0;
     reg latch = 1'b0;
     reg pulse = 1'b0;
-    reg button_lock = 1'b0;
+    reg [3:0] button_data_out = 4'b0;
+
     assign slow_clk_tb = slow_clk;
     assign latch_tb = latch;
     assign pulse_tb = pulse;
+    assign button_data_out_tb = button_data_out;
     
-    always@(posedge clk) // posedge every 25 ns
+    always@(posedge clk) // posedge every 20 ns
     begin
         slow_clk_counter <= slow_clk_counter + 19'd1;
+        
         case (slow_clk_counter)
-            19'd480: begin
+            19'd600: begin
                 latch <= 1'b0;
             end
-            19'd720: 
+            19'd900: 
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // A button pressed = 1
                 begin
                     button_data_out <= 4'b0001;
-                    button_lock < = 1'b1;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd1200:
+            19'd1500:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // B button pressed = 2
                 begin
                     button_data_out <= 4'b0010;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd1680:
+            19'd2100:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Select button pressed = 3
                 begin
                     button_data_out <= 4'b0011;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd2160:
+            19'd2700:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Start button pressed = 4
                 begin
                     button_data_out <= 4'b0100;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd2640:
+            19'd3300:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Up button pressed = 5
                 begin
                     button_data_out <= 4'b0101;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd3120:
+            19'd3900:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Down button pressed = 6
                 begin
                     button_data_out <= 4'b0110;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd3600:
+            19'd4500:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Left button pressed = 7
                 begin
                     button_data_out <= 4'b0111;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd4080:
+            19'd5100:
             begin
                 if (button_data_in == 1'b0 && button_lock == 1'b0) // Right button pressed = 8
                 begin
                     button_data_out <= 4'b1000;
+                    button_lock <= 1'b1;
                 end
                 if (slow_clk == 1'b1) 
                 begin
                     pulse <= 1'b1;
                 end
             end
-            19'd960, 19'd1440, 19'd1920, 19'd2400, 19'd2880, 19'd3360, 19'd3840, 19'd4320: 
+            19'd1200, 19'd1800, 19'd2400, 19'd3000, 19'd3600, 19'd4200, 19'd4800, 19'd5400: 
             begin
                 pulse <= 1'b0;
             end
-            19'd333333: // 60 Hz clock
+            19'd416667: // 60 Hz clock
             begin
                 if (slow_clk == 1'b0) 
                 begin
@@ -131,7 +143,13 @@ module Input_Controller (
                 end
                 slow_clk <= ~slow_clk;
                 slow_clk_counter <= 19'd0;
+                button_lock <= 1'b0;
             end
         endcase
+    end
+    
+    always@(posedge slow_clk)
+    begin
+        button_data_out <= 1'd0;
     end
 endmodule
--- Input_Controller_tb.v
+++ Input_Controller_tb.v
@@ -2,7 +2,8 @@
 
 module Input_Controller_tb;
 
-    reg clk; // 40Mhz clock
+    reg clk; // 50Mhz clock
+    reg button_data_in;
     //input reset;
     //reg button_data;
     //wire pulse;
@@ -10,24 +11,82 @@ module Input_Controller_tb;
     wire latch_tb;
     wire pulse_tb;
     //wire [18:0] slow_clk_counter_tb;
+    wire [3:0] button_data_out_tb;
+    reg [3:0] pulse_counter = 4'b0;
 
     Input_Controller uut (
         .clk(clk),                
-        //.button_data(button_data),
+        .button_data_in(button_data_in),
         .latch_tb(latch_tb),
         //.pulse(pulse),
         .slow_clk_tb(slow_clk_tb),
-        .pulse_tb(pulse_tb)
+        .pulse_tb(pulse_tb),
+        .button_data_out_tb(button_data_out_tb)
         //.slow_clk_counter_tb(slow_clk_counter_tb)
     );
 
     initial begin
         clk = 1'b0;
+        button_data_in = 1'b1;
     end
+
+    always @(posedge latch_tb)
+    begin
+        button_data_in = 1'b1; // send A button
+    end
+    always @(negedge slow_clk_tb)
+    begin
+        button_data_in = 1'b1;
+    end
+    always @(posedge pulse_tb)
+    begin
+        pulse_counter <= pulse_counter + 1'b1;
+        case (pulse_counter)
+            4'd0: 
+            begin
+                //button_data_in <= 1'b0; //send B button
+            end
+            4'd1: 
+            begin
+                //button_data_in <= 1'b0; //send Select button
+            end
+            4'd2: 
+            begin
+                //button_data_in <= 1'b0; //send Start button
+            end
+            4'd3: 
+            begin
+                //button_data_in <= 1'b0; //send Up button
+            end
+            4'd4: 
+            begin
+                //button_data_in <= 1'b0; //send Down button
+            end
+            4'd5: 
+            begin
+                //button_data_in <= 1'b0; //send Left button
+            end
+            4'd6: 
+            begin
+                //button_data_in <= 1'b0; //send Right button
+            end
+            4'd7: 
+            begin
+                button_data_in <= 1'b1; //reset data line to 1
+                pulse_counter <= 4'd0;
+            end 
+            4'd8: 
+            begin
+
+            end
+        endcase
+        button_data_in <= 1'b1;
+    end
+
 
     always @(*)
     begin
-        #125 clk <= ~clk;
+        #100 clk <= ~clk;
     end
 
 endmodule
