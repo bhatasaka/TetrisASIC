e3b67a37fc580fd7ee6dc2b3d34a04a49945d3fe
jamisonb <jamisonb@lab1-7.eng.utah.edu>
2019-11-22 20:33:05 -0700

    working on top module

--- Input_Controller.v
+++ Input_Controller.v
@@ -1,36 +1,43 @@
 module Input_Controller (
 
     input clk, // 50Mhz clock
-    //input reset,
+    input reset,
     input button_data_in,
-    output nes_reset,
-    output latch_tb,
-    output slow_clk_tb,
-    output pulse_tb,
-    output [3:0] button_data_out_tb
+    output reg nes_reset,
+    output reg [3:0] button_data_out
 
-    //output reg slow_clk
-
+    // output latch_tb,
+    // output slow_clk_tb,
+    // output pulse_tb,
+    // output [3:0] button_data_out_tb
+    
 );
     // 50Mhz clock to 60 hertz clock
     reg [18:0] slow_clk_counter = 19'd0;
-    reg [18:0] latch_clk_counter = 19'd0;
-    reg [18:0] pulse_clk_counter = 19'd0;
     reg button_lock = 1'b1;
-    reg nes_reset = 1'b0;
 
     reg slow_clk = 1'b0;
     reg latch = 1'b0;
     reg pulse = 1'b0;
-    reg [3:0] button_data_out = 4'b0;
+    //reg [3:0] button_data_out = 4'b0;
 
-    assign slow_clk_tb = slow_clk;
-    assign latch_tb = latch;
-    assign pulse_tb = pulse;
-    assign button_data_out_tb = button_data_out;
+    // assign slow_clk_tb = slow_clk;
+    // assign latch_tb = latch;
+    // assign pulse_tb = pulse;
+    // assign button_data_out_tb = button_data_out;
     
     always@(posedge clk) // posedge every 20 ns
     begin
+        if (reset)
+        begin
+            slow_clk_counter <= 19'd0;
+            button_lock <= 1'b1;
+            nes_reset <= 1'b0;
+            slow_clk <= 1'b0;
+            latch <= 1'b0;
+            pulse <= 1'b0;
+            button_data_out <= 4'b0;
+        end
         slow_clk_counter <= slow_clk_counter + 19'd1;
         
         case (slow_clk_counter)
@@ -40,7 +47,7 @@ module Input_Controller (
             end
             19'd900: //posedge of pulse1
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // A button pressed = 1
+                if (~button_data_in && ~button_lock) // A button pressed = 1
                 begin
                     button_data_out <= 4'b0001;
                     button_lock <= 1'b1;
@@ -52,7 +59,7 @@ module Input_Controller (
             end
             19'd1500: // posedge pulse2
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // B button pressed = 2
+                if (~button_data_in && ~button_lock) // B button pressed = 2
                 begin
                     button_data_out <= 4'b0010;
                     button_lock <= 1'b1;
@@ -64,7 +71,7 @@ module Input_Controller (
             end
             19'd2100: //posedge pulse3
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Select button pressed = 3
+                if (~button_data_in && ~button_lock) // Select button pressed = 3
                 begin
                     button_data_out <= 4'b0011;
                     button_lock <= 1'b1;
@@ -76,7 +83,7 @@ module Input_Controller (
             end
             19'd2700: //posedge pulse4
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Start button pressed = 4
+                if (~button_data_in && ~button_lock) // Start button pressed = 4
                 begin
                     button_data_out <= 4'b0100;
                     button_lock <= 1'b1;
@@ -89,7 +96,7 @@ module Input_Controller (
             end
             19'd3300: //posedge pulse5
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Up button pressed = 5
+                if (~button_data_in && ~button_lock) // Up button pressed = 5
                 begin
                     button_data_out <= 4'b0101;
                     button_lock <= 1'b1;
@@ -101,7 +108,7 @@ module Input_Controller (
             end
             19'd3900: //posedge pulse6
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Down button pressed = 6
+                if (~button_data_in && ~button_lock) // Down button pressed = 6
                 begin
                     button_data_out <= 4'b0110;
                     button_lock <= 1'b1;
@@ -113,7 +120,7 @@ module Input_Controller (
             end
             19'd4500: //posedge pulse7
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Left button pressed = 7
+                if (~button_data_in && ~button_lock) // Left button pressed = 7
                 begin
                     button_data_out <= 4'b0111;
                     button_lock <= 1'b1;
@@ -125,7 +132,7 @@ module Input_Controller (
             end
             19'd5100: //posedge pulse8
             begin
-                if (button_data_in == 1'b0 && button_lock == 1'b0) // Right button pressed = 8
+                if (~button_data_in && ~button_lock) // Right button pressed = 8
                 begin
                     button_data_out <= 4'b1000;
                     button_lock <= 1'b1;
@@ -141,19 +148,15 @@ module Input_Controller (
             end
             19'd416667: // 60 Hz clock
             begin
-                if (slow_clk == 1'b0) // if on rising edge of slow clock send latch signal
+                if (~slow_clk) // if on rising edge of slow clock send latch signal
                 begin
                     latch <= 1'b1;
+                    button_data_out <= 1'd0; 
                 end
                 slow_clk <= ~slow_clk;
                 slow_clk_counter <= 19'd0;
                 button_lock <= 1'b0;
             end
         endcase
-    end
-    
-    always@(posedge slow_clk)
-    begin
-        button_data_out <= 1'd0; // move this to when latch<=1'b1 right above and add reset functionality
     end
 endmodule
--- Tetris.v
+++ Tetris.v
@@ -1,25 +1,70 @@
 module Tetris (
 
-    input clock,
+    input clk,
     input hard_reset,
-    input nes_reset,
-    output reset,
-    output clock
+    input nes_in
+    //vga outputs
 
-);
+    );
 
-always @(posedge clock)
-begin
-    if (hard_reset == 1'b1 || nes_reset == 1'b1)
+    reg reset;
+    wire nes_reset, grid_we;
+    wire [7:0] button_data, grid_address, data_from_grid, data_to_grid;
+    
+
+    always @(posedge clk)
     begin
-        reset <= 1'b1;
+        if (hard_reset|| nes_reset)
+        begin
+            reset <= 1'b1;
+        end
+        else
+        begin
+            reset <= 1'b0;
+        end
     end
-    else
-    begin
-        reset <= 1'b0;
-    end
-end
 
+    Input_Controller input_controller (
+        .clk(clk), // 50Mhz clock
+        .reset(reset),
+        .button_data_in(nes_in),
+        .nes_reset(nes_reset),
+        .button_data_out(button_data)
+    );
+
+    Grid_Controller grid_controller (
+        .clk(clk),
+        .reset(reset),
+        .controller_in(button_data),
+        .tetris_grid_in(data_from_grid),
+        .grid_address(grid_address),
+        .tetris_grid_out(data_to_grid),
+        .write_en(grid_we)
+    );
+
+    Grid_Mem grid_mem (
+        .clk(clk),
+        .data_a(data_to_grid),
+        .addr_a(grid_address), 
+        .addr_b(),
+        .we_a(grid_we), 
+        .q_a(data_from_grid), 
+        .q_b()
+    );
+
+    VGA_Mem vga_mem (
+        .clk(clk),
+        .data_a(),
+        .addr_a(), 
+        .addr_b(),
+        .we_a(), 
+        .q_a(), 
+        .q_b()
+    );
+
+    Grid_To_Video grid_to_video (
+
+    );
 
 
 
