1a504332172b25cd1783600122929ca1583d105a
jamisonb <jamisonb@lab1-18.eng.utah.edu>
2019-11-24 11:36:23 -0700

    button parameters.

--- Grid_Controller.v
+++ Grid_Controller.v
@@ -17,7 +17,7 @@ module Grid_Controller (
 		output reg [7:0] tetris_grid_out,
 		output reg write_en
 );
-	parameter [3:0] s_start = 4'b0000, s_reset = 4'b0001, s_address = 4'b0010, s_data = 4'b0011, s4 = 4'b0100, 
+	parameter [3:0] s_address = 4'b0000, s_data = 4'b0001, s_2 = 4'b0010, s_3 = 4'b0011, s4 = 4'b0100, 
 						  s5 = 4'b0101, s6 = 4'b0110, s7 = 4'b0111, s8 = 4'b1000, s9 = 4'b1001,
 						  s10 = 4'b1010, s11 = 4'b1011, s12 = 4'b1100, s13 = 4'b1101;
 	
