4eb5899646688dd108070e7ba60ffc1da1f94021
jamisonb <jamisonb@lab2-29.eng.utah.edu>
2019-12-02 14:32:33 -0700

    edited synthesis script file and added pads in Tetris_Top

--- Tetris_Top.v
+++ Tetris_Top.v
@@ -1,5 +1,4 @@
 `include "/research/ece/lnis-teaching/5710_6710/Lab_files/HDL/padlib_tsmc180_innovus.v"
-`include "Tetris_Mapped.v"
 
 module Tetris_Top (
     input clk,
@@ -12,31 +11,68 @@ module Tetris_Top (
 	output wire v_sync
 );
 
-pad_in pad_in_clk (.pad(clk), .DataIn(clk_pad));
-pad_in pad_in_reset (.pad(hard_reset), .DataIn(hard_reset_pad));
-pad_in pad_in_nes (.pad(nes_in), .DataIn(nes_in_pad));
+    // input pads
+    pad_in pad_in_clk (.pad(clk), .DataIn(clk_pad));
+    pad_in pad_in_reset (.pad(hard_reset), .DataIn(hard_reset_pad));
+    pad_in pad_in_nes (.pad(nes_in), .DataIn(nes_in_pad));
 
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
-pad_out_buffered pad_out0 (.pad(r_out[0]), .out(r_out[0]_pad));
+    // output pads
 
+    // r_out
+    pad_out_buffered pad_out_r0 (.pad(r_out[0]), .out(r_out_pad[0]));
+    pad_out_buffered pad_out_r1 (.pad(r_out[1]), .out(r_out_pad[1]));
+    pad_out_buffered pad_out_r2 (.pad(r_out[2]), .out(r_out_pad[2]));
+    pad_out_buffered pad_out_r3 (.pad(r_out[3]), .out(r_out_pad[3]));
+    pad_out_buffered pad_out_r4 (.pad(r_out[4]), .out(r_out_pad[4]));
+    pad_out_buffered pad_out_r5 (.pad(r_out[5]), .out(r_out_pad[5]));
+    pad_out_buffered pad_out_r6 (.pad(r_out[6]), .out(r_out_pad[6]));
+    pad_out_buffered pad_out_r7 (.pad(r_out[7]), .out(r_out_pad[7]));
 
+    // g_out
+    pad_out_buffered pad_out_g0 (.pad(g_out[0]), .out(g_out_pad[0]));
+    pad_out_buffered pad_out_g1 (.pad(g_out[1]), .out(g_out_pad[1]));
+    pad_out_buffered pad_out_g2 (.pad(g_out[2]), .out(g_out_pad[2]));
+    pad_out_buffered pad_out_g3 (.pad(g_out[3]), .out(g_out_pad[3]));
+    pad_out_buffered pad_out_g4 (.pad(g_out[4]), .out(g_out_pad[4]));
+    pad_out_buffered pad_out_g5 (.pad(g_out[5]), .out(g_out_pad[5]));
+    pad_out_buffered pad_out_g6 (.pad(g_out[6]), .out(g_out_pad[6]));
+    pad_out_buffered pad_out_g7 (.pad(g_out[7]), .out(g_out_pad[7]));
 
+    // b_out
+    pad_out_buffered pad_out_b0 (.pad(b_out[0]), .out(b_out_pad[0]));
+    pad_out_buffered pad_out_b1 (.pad(b_out[1]), .out(b_out_pad[1]));
+    pad_out_buffered pad_out_b2 (.pad(b_out[2]), .out(b_out_pad[2]));
+    pad_out_buffered pad_out_b3 (.pad(b_out[3]), .out(b_out_pad[3]));
+    pad_out_buffered pad_out_b4 (.pad(b_out[4]), .out(b_out_pad[4]));
+    pad_out_buffered pad_out_b5 (.pad(b_out[5]), .out(b_out_pad[5]));
+    pad_out_buffered pad_out_b6 (.pad(b_out[6]), .out(b_out_pad[6]));
+    pad_out_buffered pad_out_b7 (.pad(b_out[7]), .out(b_out_pad[7]));
 
-pad_vdd pad_vdd0 ();
-pad_gnd pad_gnd0 ();
+    // h_sync
+    pad_out_buffered pad_out_hsync (.pad(h_sync), .out(h_sync_pad));
 
+    // v_sync
+    pad_out_buffered pad_out_vsync (.pad(v_sync), .out(v_sync_pad));
 
-pad_corner corner0 ();
-pad_corner corner1 ();
-pad_corner corner2 ();
-pad_corner corner3 ();
+    // vdd and gnd pads
+    pad_vdd pad_vdd0 ();
+    pad_gnd pad_gnd0 ();
 
+    // corner pads
+    pad_corner corner0 ();
+    pad_corner corner1 ();
+    pad_corner corner2 ();
+    pad_corner corner3 ();
 
-
-
+    // post synthesis verilog file
+    Tetris_Mapped tetris_mapped (
+        .clk(clk_pad),
+        .hard_reset(hard_reset_pad),
+        .nes_in(nes_in_pad),
+        .r_out(r_out_pad),
+        .g_out(g_out_pad),
+        .b_out(b_out_pad),
+        .h_sync(h_sync_pad),
+        .v_sync(v_sync_pad)
+    );
+endmodule
--- synthesis.tcl
+++ synthesis.tcl
@@ -1,6 +1,7 @@
-# Optimization settings
 # The period has to be changed to see if the design can meet the timing constraint
-set period 20
+#Same for the clk_name when using a different design.
+set clk_period 20
+set clk_name clk
 
 #  Define the names to be used by the script
 set design Tetris
@@ -27,38 +28,51 @@ $RTL_DIR/VGA_Controller.v \
 $RTL_DIR/VGA_Mem.v ]
 
 
+#Create the directories if they do no exist
+exec mkdir -p $RPT_DIR
+exec mkdir -p $DDC_DIR
+exec mkdir -p $SDF_DIR
+exec mkdir -p $SDC_DIR
+exec mkdir -p $DESIGN_LIB
 
 #  Start from fresh state
 remove_design -designs
 
+#Define a design library specific to the current design. Otherwise, when runing 2 synthesis in parallel, intermediate files get mixed.
+define_design_lib $design -path $DESIGN_LIB
+
 #  Analyze the Verilog sources
 puts "-i- Analyze Verilog sources"
-analyze -format sverilog $HDL_FILES
+analyze -format sverilog $HDL_FILES -library $design
 
 #  Elaborate the design
 puts "-i- Elaborate design"
-elaborate ${design} -update -library DEFAULT
+elaborate ${design} -library $design
 
 #  Save the elaborated design
 puts "-i- Save elaborated design"
 write -hierarchy -format ddc -output ${DDC_DIR}/${design}_elab.ddc
+
+#  Link the design
+puts "-i- Linking the design"
+link
 
 #  Define constraints
 puts "-i- Define constraints"
 puts "-i- set_max_area 0"
 puts "-i- set_clock"
 set_max_area 0
-create_clock -name "clk" -period $period clk
+create_clock -name $clk_name -period $clk_period $clk_name
 
 #Check the design for warnings
 check_design
 
 #  Do not ungroup the hierarchy
-set_ungroup [get_designs *] false
+#set_ungroup [get_designs *] false
 
 #  Map and optimize the design
 puts "-i- Map and optimize design"
-compile_ultra
+compile
 
 #  Save the mapped design
 puts "-i- Save mapped design"
@@ -73,14 +87,14 @@ report_power -nosplit -analysis_effort low > ${RPT_DIR}/${design}_mapped_power.r
 
 #  Generate the Verilog netlist
 puts "-i- Generate Verilog netlist"
-write -format verilog -hierarchy -output ${GATE_DIR}/${design}_mapped.v
+write_file -format verilog -hierarchy -output ${GATE_DIR}/${design}_mapped.v
 
 #  Generate the design constraint file
 puts "-i- Generate SDC design constraint file"
 write_sdc -nosplit ${SDC_DIR}/${design}_mapped.sdc
 
-#  Save the synthesized design
-puts "-i- Save synthesized design"
-write -hierarchy -format ddc -output ${DDC_DIR}/${design}_synthesized.ddc
+#  Generate the sdf file
+puts "-i- Generate the SDF file"
+write_sdf -version 2.1 ${SDF_DIR}/${design}_mapped.sdf
 
 puts "-i- Finished"
